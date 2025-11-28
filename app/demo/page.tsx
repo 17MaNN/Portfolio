b16c268492576f08547c08fab5ca58b1c51400aa
17MaNN <im.in.your.mann@gmail.com>
2025-11-28 17:00:01 +0530

    portfolio structure created

---
+++ app/demo/page.tsx
@@ -0,0 +1,9 @@
+import { LiquidEffectAnimation } from "@/components/ui/liquid-effect-animation";
+
+export default function DemoPage() {
+  return (
+    <div className="w-full h-screen">
+      <LiquidEffectAnimation />
+    </div>
+  );
+}
